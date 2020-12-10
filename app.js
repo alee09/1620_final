7d59b16ebed6a3990ad3017cbbba7375e71baea0
Adrian Lee <adl0924@gmail.com>
2020-12-10 02:49:12 -0800

    finished css

---
+++ app.js
@@ -0,0 +1,8 @@
+const interactive = document.querySelector(".interactive")
+const thumb_bar = documents.querySelector(".thumb-bar")
+const focused = document.querySelector(".focused_image")
+const button = document.querySelector(".btn")
+
+function showimg(){
+    for(let i =1; i<5; i++)
+}
